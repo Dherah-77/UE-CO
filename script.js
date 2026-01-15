f7193e47f068305c4a2efddef41c5a43de305c1d
Dherah-77 <dherah911@gmail.com>
2026-01-15 17:30:56 +0100

    mod

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Image change
 const styleImages = [
   "./Images/BAG2-scaled.jpg",
   "./Images/Gallery2.jpg",
@@ -13,3 +14,25 @@ setInterval(() => {
   currentIndex = (currentIndex + 1) % styleImages.length;
   styleImageElement.src = styleImages[currentIndex];
 }, 4000);
+
+// Sliding Images
+
+const track = document.getElementById("track");
+
+const slideWidth = 240 + 16; // w-60 (240px) + gap-4 (16px)
+const pause = 3000; // 1.8 seconds pause
+const move = 1500; // slide duration
+
+function slide() {
+  track.style.transform = `translateX(-${slideWidth}px)`;
+
+  setTimeout(() => {
+    track.appendChild(track.firstElementChild);
+    track.style.transition = "none";
+    track.style.transform = "translateX(0)";
+    track.offsetHeight; // force repaint
+    track.style.transition = "transform 1s ease-in-out";
+  }, move);
+}
+
+setInterval(slide, pause + move);
